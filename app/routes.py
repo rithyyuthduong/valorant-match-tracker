6d18e9ee94a44a7e6d8eb973fba5371ab96b70a1
RITHY YUTH DUONG <rithyyuthd@gmail.com>
2026-04-27 18:41:55 +1000

    added index route to query for list of matches

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,21 @@
-from app import app
+from app import app, db
+from flask import render_template, url_for, redirect, flash
+import sqlalchemy as sa
+from app.models import Match
+from app.forms import MatchForm
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET'])
 def index():
-    return 'Valorant Tracker - Coming soon.'
+    matches = db.session.scalars(sa.select(Match)).all()
+    return render_template('index.html', title='Match History', matches=matches)
+        
+@app.route('/add_match', methods=['GET', 'POST'])
+def add_match():
+    form = MatchForm()
+    if form.validate_on_submit():
+        match = db.session.scalar(sa.select(Match)).all()
+        db.session.add(match)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('index.html', title='Add Match', form=form)
