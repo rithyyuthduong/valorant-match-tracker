0d271c4d28adc49486f4008468f29258018a9e20
RITHY YUTH DUONG <rithyyuthd@gmail.com>
2026-04-27 18:44:58 +1000

    added add_match route to log matches

--- app/routes.py
+++ app/routes.py
@@ -14,8 +14,21 @@ def index():
 def add_match():
     form = MatchForm()
     if form.validate_on_submit():
-        match = db.session.scalar(sa.select(Match)).all()
+        match = Match(
+            agent=form.agent.data,
+            kills=form.kills.data,
+            deaths=form.deaths.data,
+            assists=form.assists.data,
+            result=form.result.data,
+            map=form.map.data,
+            my_team_score=form.my_team_score.data,
+            enemy_team_score=form.enemy_team_score.data,
+            rank_before=form.rank_before.data,
+            rank_after=form.rank_after.data,
+            rr_change=form.rr_change.data
+        )
         db.session.add(match)
         db.session.commit()
+        flash('Match logged successfully!')
         return redirect(url_for('index'))
-    return render_template('index.html', title='Add Match', form=form)
+    return render_template('add_match.html', title='Add Match', form=form)
